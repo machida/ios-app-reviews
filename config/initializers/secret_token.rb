e829a8addfe4784a78a97acdc4ffd9ab0c888bf6
nomnel <laxyz.nomnel@gmail.com>
2013-07-13 13:49:22 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosAppReviews::Application.config.secret_key_base = '53c5d54fd10d71a7c675636ebc426a54d75344bc7f5fc736d0d2f7281c0e9597ef44dc0838c37ac2586d6fce2a32e2a52225c30267b5a6018b531d382c52b598'
