f5f2fd1936acf55b0864cbf7dee1e07cee4df3c8
nomnel <laxyz.nomnel@gmail.com>
2013-07-24 14:04:26 +0900

    remove extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 IosAppReviews::Application.routes.draw do
-
-  resources :reviewers, only: [:index, :show] do
-    get 'reviews' => 'reviews#by_reviewer'
-  end
-  resources :reviews, only: [:index, :show] do
+  root 'reviews#index'
+  resources :reviews, only: :index do
     get 'page/:page', action: :index, on: :collection
   end
-  resources :apps, only: [:index, :show]
-
 end
